5885aa72eb6e1d001cb32a621f4aa6e87bb1b677
disreal48 <disreal48@googlemail.com>
2022-04-24 18:48:08 +0200

    finished foundation

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 const Button = ({ color, text, onClick }) => {
   return (
-    <button onClick={onClick} style={{ color: color }} className='btn'>
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
       {text}
     </button>
-  );
-};
+  )
+}
 
-Button.defautProps = {
+Button.defaultProps = {
   color: 'steelblue',
-};
+}
 
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
   onClick: PropTypes.func,
-};
+}
 
-export default Button;
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,36 @@
-import PropTypes from 'prop-types';
-import Button from './Button';
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+import Button from './Button'
 
-const Header = ({ title }) => {
-const onClick = () => {
-  console.log('Click')
-}
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
 
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick}/>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
     </header>
-  );
-};
+  )
+}
 
-Header.defautProps = {
-    title: 'Task Tracker',
+Header.defaultProps = {
+  title: 'Task Tracker',
 }
 
 Header.propTypes = {
-    title: PropTypes.string,
+  title: PropTypes.string.isRequired,
 }
 
-const headingStyle = {
-    color: 'red', 
-    backgroundColor: 'black '
-}
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
+// }
 
-export default Header;
+export default Header
